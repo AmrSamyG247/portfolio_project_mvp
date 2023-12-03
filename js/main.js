b5e12b0f83db270556596285f07509684cc08248
AmrSamyG247 <86115819+AmrSamyG247@users.noreply.github.com>
2023-12-03 12:09:31 +0200

    Add btt option

--- js/main.js
+++ js/main.js
@@ -27,3 +27,24 @@ $(document).ready(function(){
   });
 
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopButton = document.getElementById("back-to-top-btn");
+
+    // Show the button when scrolling down
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    };
+});
+
+// Scroll to top function
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
